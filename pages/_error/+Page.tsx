76252d3413361936b2f32f6a3e2a59d5a5f02e7a
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-06-21 21:28:01 +0300

    Updated error page

--- pages/_error/+Page.tsx
+++ pages/_error/+Page.tsx
@@ -7,16 +7,24 @@ export default function Page() {
 	if (is404) {
 		return (
 			<>
-				<h1>{getText('pageError.notFound.title')}</h1>
-				<p>{getText('pageError.notFound.text')}</p>
+				<div className="grid h-dvh place-items-center">
+					<div className="text-center">
+						<h1 className="mb-1 large-font">{getText('pageError.notFound.title')}</h1>
+						<p className="small-font">{getText('pageError.notFound.text')}</p>
+					</div>
+				</div>
 			</>
 		);
 	}
 
 	return (
 		<>
-			<h1>{getText('pageError.other.title')}</h1>
-			<p>{getText('pageError.other.text')}</p>
+			<div className="grid h-dvh place-items-center">
+				<div className="text-center">
+					<h1 className="mb-1 large-font">{getText('pageError.other.title')}</h1>
+					<p className="small-font">{getText('pageError.other.text')}</p>
+				</div>
+			</div>
 		</>
 	);
 }
