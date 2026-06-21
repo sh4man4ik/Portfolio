830010b8c59836015a6b639554b2975b6c5cb6f1
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-06-21 16:29:49 +0300

    Added background blur for StartChat

---
+++ components/Blur.tsx
@@ -0,0 +1,7 @@
+export default function Blur() {
+	return (
+		<>
+			<div className="absolute inset-0 m-auto bg-[#8E3E63] blur-3xl z-[-1] max-w-[500px] w-[70%] h-[70px] top-[70px] opacity-70"></div>
+		</>
+	);
+}
--- modules/StartChat/components/Input.tsx
+++ modules/StartChat/components/Input.tsx
@@ -23,8 +23,8 @@ export default function Input() {
 						<option>{getText('startChat.select.options.fourth')}</option>
 					</select>
 
-					<button className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-[#FF97D0] backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 border-none">
-						<RiArrowUpLine size={18} color="rgba(0,0,0,1)" />
+					<button className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-lg bg-[#8E3E63] px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 border-none">
+						<RiArrowUpLine size={18} />
 						<div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
 							<div className="relative h-full w-10 bg-white/20"></div>
 						</div>
