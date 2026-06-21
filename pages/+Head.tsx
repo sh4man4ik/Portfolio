ab6be2c67fa5582eb6b0a04d7243a6ff71033111
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-06-21 13:14:07 +0300

    Added Inter font

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -3,6 +3,13 @@
 export function Head() {
 	return (
 		<>
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+			<link
+				href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+				rel="stylesheet"
+			/>
+
 			<script
 				async
 				src={`https://www.googletagmanager.com/gtag/js?id=${import.meta.env.PUBLIC_ENV__GOOGLE_ANALYTICS}`}
