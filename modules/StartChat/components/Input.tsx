a357e51365d4309eff9f7731eda8d5c16c067c0e
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-06-24 13:44:11 +0300

    Added worked enter button (send)

--- modules/StartChat/components/Input.tsx
+++ modules/StartChat/components/Input.tsx
@@ -13,40 +13,42 @@ export default function Input(props: any) {
 
 	return (
 		<>
-			<div className="grid pt-[10px] p-[15px] bg-base-200 rounded-xl gap-[15px] max-w-[500px] w-[80%] mb-[20px] shadow-[0px_-3px_25px_6px_rgba(0,_0,_0,_0.6)]">
-				<input
-					type="text"
-					placeholder={getText('startChat.input.placeholder')}
-					className="input border-none outline-none bg-transparent medium-font w-full"
-					suppressHydrationWarning={true}
-					value={props.inputValue}
-					onChange={(event) => props.setInputValue(event.target.value)}
-				/>
-				<div className="flex justify-between">
-					<select
-						className="select outline-none border-none w-fit rounded-lg small-font bg-base-300"
+			<form className="w-full grid place-items-center" onSubmit={(e) => e.preventDefault()}>
+				<div className="grid pt-[10px] p-[15px] bg-base-200 rounded-xl gap-[15px] max-w-[500px] w-[80%] mb-[20px] shadow-[0px_-3px_25px_6px_rgba(0,_0,_0,_0.6)]">
+					<input
+						type="text"
+						placeholder={getText('startChat.input.placeholder')}
+						className="input border-none outline-none bg-transparent medium-font w-full"
 						suppressHydrationWarning={true}
-						value={props.selectValue}
-						onChange={(event) => props.setSelectValue(event.target.value)}
-					>
-						<option>{getText('startChat.select.options.first')}</option>
-						<option>{getText('startChat.select.options.second')}</option>
-						<option>{getText('startChat.select.options.third')}</option>
-						<option>{getText('startChat.select.options.fourth')}</option>
-					</select>
+						value={props.inputValue}
+						onChange={(event) => props.setInputValue(event.target.value)}
+					/>
+					<div className="flex justify-between">
+						<select
+							className="select outline-none border-none w-fit rounded-lg small-font bg-base-300"
+							suppressHydrationWarning={true}
+							value={props.selectValue}
+							onChange={(event) => props.setSelectValue(event.target.value)}
+						>
+							<option>{getText('startChat.select.options.first')}</option>
+							<option>{getText('startChat.select.options.second')}</option>
+							<option>{getText('startChat.select.options.third')}</option>
+							<option>{getText('startChat.select.options.fourth')}</option>
+						</select>
 
-					<button
-						className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-lg bg-[#8E3E63] px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 border-none"
-						suppressHydrationWarning={true}
-						onClick={props.handleClick}
-					>
-						<RiArrowUpLine size={18} />
-						<div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
-							<div className="relative h-full w-10 bg-white/20"></div>
-						</div>
-					</button>
+						<button
+							className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-lg bg-[#8E3E63] px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 border-none"
+							suppressHydrationWarning={true}
+							onClick={props.handleClick}
+						>
+							<RiArrowUpLine size={18} />
+							<div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
+								<div className="relative h-full w-10 bg-white/20"></div>
+							</div>
+						</button>
+					</div>
 				</div>
-			</div>
+			</form>
 		</>
 	);
 }
